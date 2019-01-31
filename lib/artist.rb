87a4d68e90d14d263c97c2e0d5b57ef761f2a904
Jessica Poe <jesspoe00@gmail.com>
2019-01-30 23:56:44 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require "pry"
+class Artist
+attr_accessor :name, :songs
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @songs = []
+  @@all << self
+end
+
+def add_song(song)
+  self.songs << song
+  song.artist = self
+end
+
+def add_song_by_name(name)
+  song = Song.new(name)
+  self.songs << song
+  song.artist = self
+end
+
+def self.song_count
+Song.all.length
+end
+
+
+end # End Artist class
---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+attr_accessor :name, :posts
+@@all = []
+
+def initialize(name)
+  @name = name
+  @posts = []
+  @@all << self
+end
+
+def posts
+  @posts
+end
+
+def add_post(post)
+  post.author = self
+    @posts << post
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  @posts<< post
+  post.author = self
+end
+
+def self.post_count
+  Post.all.length
+end
+
+end # end of author class
